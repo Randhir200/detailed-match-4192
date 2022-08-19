baf401cb97b942e2ec22eb39702aecb31879133f
Randhir200 <mail.randhirkr@gmail.com>
2022-08-19 11:11:52 +0530

    server url changes

--- dotnkey-clone/src/api/api.js
+++ dotnkey-clone/src/api/api.js
@@ -4,28 +4,28 @@ import axios from 'axios';
 
 export async function getData(filter='ratings_gte=4&ratings_lte=5',sort='_sort=price&_order=asc') {
   let res = await axios.get(
-    `http://localhost:8000/products?${filter}&${sort}`
+    `https://dot-n-key-server.herokuapp.com/products?${filter}&${sort}`
   );
   return res.data;
 }
 
 export async function postCartData(data) {
   axios({
-    url: 'http://localhost:8000/cart',
+    url: 'https://dot-n-key-server.herokuapp.com/cart',
     method: 'POST',
     data,
   });
 }
 export async function patchCartData(data, id) {
   axios({
-    url: `http://localhost:8000/cart/${id}`,
+    url: `https://dot-n-key-server.herokuapp.com/cart/${id}`,
     method: 'PATCH',
     data,
   });
 }
 export async function getCartData() {
   let res = await axios({
-    url: 'http://localhost:8000/cart',
+    url: 'https://dot-n-key-server.herokuapp.com/cart',
     method: 'GET',
   });
   return res.data;
@@ -33,26 +33,26 @@ export async function getCartData() {
 
 export async function deleteCartData(id){
   let res = await axios({
-    url: `http://localhost:8000/cart/${id}`,
+    url: `https://dot-n-key-server.herokuapp.com/cart/${id}`,
     method: 'DELETE',
   });
 }
 export async function postUsersData(data) {
   axios({
-    url: 'http://localhost:8000/users',
+    url: 'https://dot-n-key-server.herokuapp.com/users',
     method: 'POST',
     data,
   });
 }
 export async function getUsersData() {
   let res = await axios({
-    url: 'http://localhost:8000/users',
+    url: 'https://dot-n-key-server.herokuapp.com/users',
   });
   return res.data;
 }
 export async function postSigninData(data) {
   axios({
-    url: 'http://localhost:8000/signinData',
+    url: 'https://dot-n-key-server.herokuapp.com/signinData',
     method: 'POST',
     data,
   });
